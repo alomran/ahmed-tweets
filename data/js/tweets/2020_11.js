fe2111d2b815169ba9c197af407b609d2ced09dd
alomran <ahmed@alomran.me>
2020-11-02 21:17:10 +0000

    Added 3 tweets

---
+++ data/js/tweets/2020_11.js
@@ -0,0 +1,450 @@
+Grailbird.data.tweets_2020_11 = 
+[
+	{
+		"created_at": "Mon Nov 02 20:03:51 +0000 2020",
+		"id": 1323355159834071000,
+		"id_str": "1323355159834071041",
+		"full_text": "Election Unrest Threatens US as Regime Supporters and Opposition Head to Polls https://t.co/oKnNWavcre",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			102
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/oKnNWavcre",
+					"expanded_url": "https://www.nytimes.com/2020/11/02/nyregion/nyc-election-unrest.html",
+					"display_url": "nytimes.com/2020/11/02/nyr…",
+					"indices": [
+						79,
+						102
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 5242,
+			"id_str": "5242",
+			"name": "Ahmed Al Omran",
+			"screen_name": "ahmed",
+			"location": "the internet",
+			"description": "Saudi journalist · Formerly with @FT, @WSJ and @NPR · Email: alomran@hey.com · Subscribe to my @riyadhbureau newsletter: https://t.co/i1MM94SRXS",
+			"url": "http://t.co/1tckurrksd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/1tckurrksd",
+							"expanded_url": "http://alomran.me",
+							"display_url": "alomran.me",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/i1MM94SRXS",
+							"expanded_url": "http://riyadhbureau.com",
+							"display_url": "riyadhbureau.com",
+							"indices": [
+								121,
+								144
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 150803,
+			"friends_count": 1950,
+			"listed_count": 2386,
+			"created_at": "Mon Sep 04 00:18:12 +0000 2006",
+			"favourites_count": 5329,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 105792,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "CCCCCC",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1175469297675714562/QUWgZ3Gk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1175469297675714562/QUWgZ3Gk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/5242/1569089262",
+			"profile_link_color": "002FA7",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "99CC33",
+			"profile_text_color": "3E4415",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Election Unrest Threatens US as Regime Supporters and Opposition Head to Polls https://t.co/oKnNWavcre"
+	},
+	{
+		"created_at": "Mon Nov 02 20:03:18 +0000 2020",
+		"id": 1322169947032748000,
+		"id_str": "1323355021740810241",
+		"full_text": ".@ahmed leaving the @FT to focus on his @riyadhbureau newsletter: https://t.co/b4bBvlGwEX https://t.co/lrjTvZAUO6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			89
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ahmed",
+					"name": "Ahmed Al Omran",
+					"id": 5242,
+					"id_str": "5242",
+					"indices": [
+						1,
+						7
+					]
+				},
+				{
+					"screen_name": "FT",
+					"name": "Financial Times",
+					"id": 18949452,
+					"id_str": "18949452",
+					"indices": [
+						20,
+						23
+					]
+				},
+				{
+					"screen_name": "riyadhbureau",
+					"name": "Riyadh Bureau",
+					"id": 7686182,
+					"id_str": "7686182",
+					"indices": [
+						40,
+						53
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/b4bBvlGwEX",
+					"expanded_url": "https://talkingbiznews.com/they-talk-biz-news/ft-reporter-al-omran-leaving-to-focus-on-newsletter/",
+					"display_url": "talkingbiznews.com/they-talk-biz-…",
+					"indices": [
+						66,
+						89
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1322169938920968200,
+					"id_str": "1322169938920968192",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/EllKjeYVkAAkVBQ.png",
+					"media_url_https": "https://pbs.twimg.com/media/EllKjeYVkAAkVBQ.png",
+					"url": "https://t.co/lrjTvZAUO6",
+					"display_url": "pic.twitter.com/lrjTvZAUO6",
+					"expanded_url": "https://twitter.com/talkingbiznews/status/1322169947032748037/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 358,
+							"h": 224,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 358,
+							"h": 224,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 358,
+							"h": 224,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1322169938920968200,
+					"id_str": "1322169938920968192",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/EllKjeYVkAAkVBQ.png",
+					"media_url_https": "https://pbs.twimg.com/media/EllKjeYVkAAkVBQ.png",
+					"url": "https://t.co/lrjTvZAUO6",
+					"display_url": "pic.twitter.com/lrjTvZAUO6",
+					"expanded_url": "https://twitter.com/talkingbiznews/status/1322169947032748037/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 358,
+							"h": 224,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 358,
+							"h": 224,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 358,
+							"h": 224,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16892243,
+			"id_str": "16892243",
+			"name": "Chris Roush",
+			"screen_name": "talkingbiznews",
+			"location": "Hamden, CT",
+			"description": "Biz journalism news, information and job openings for pens in pursuit of the bottom line. Black Lives Matter. Founded by @QUsocdean.",
+			"url": "http://t.co/bsSxLTfsTG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/bsSxLTfsTG",
+							"expanded_url": "http://www.talkingbiznews.com",
+							"display_url": "talkingbiznews.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10547,
+			"friends_count": 4339,
+			"listed_count": 471,
+			"created_at": "Tue Oct 21 19:38:19 +0000 2008",
+			"favourites_count": 4746,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 49694,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "234B7C",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme19/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme19/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1135591381458538496/o9iVUgyX_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1135591381458538496/o9iVUgyX_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16892243/1535461691",
+			"profile_link_color": "234B7C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "F6FFD1",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 5,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": ".@ahmed leaving the @FT to focus on his @riyadhbureau newsletter: https://t.co/b4bBvlGwEX https://t.co/lrjTvZAUO6"
+	},
+	{
+		"created_at": "Mon Nov 02 19:54:35 +0000 2020",
+		"id": 1323352827448725500,
+		"id_str": "1323352827448725504",
+		"full_text": "SoftBank’s Saudi-backed Vision Fund loses COO and four other partners https://t.co/fIpUXvSb5W",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fIpUXvSb5W",
+					"expanded_url": "https://www.bloomberg.com/news/articles/2020-11-02/softbank-s-100-billion-vision-fund-loses-coo-four-partners",
+					"display_url": "bloomberg.com/news/articles/…",
+					"indices": [
+						70,
+						93
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 5242,
+			"id_str": "5242",
+			"name": "Ahmed Al Omran",
+			"screen_name": "ahmed",
+			"location": "the internet",
+			"description": "Saudi journalist · Formerly with @FT, @WSJ and @NPR · Email: alomran@hey.com · Subscribe to my @riyadhbureau newsletter: https://t.co/i1MM94SRXS",
+			"url": "http://t.co/1tckurrksd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/1tckurrksd",
+							"expanded_url": "http://alomran.me",
+							"display_url": "alomran.me",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/i1MM94SRXS",
+							"expanded_url": "http://riyadhbureau.com",
+							"display_url": "riyadhbureau.com",
+							"indices": [
+								121,
+								144
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 150803,
+			"friends_count": 1950,
+			"listed_count": 2386,
+			"created_at": "Mon Sep 04 00:18:12 +0000 2006",
+			"favourites_count": 5329,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 105792,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "CCCCCC",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1175469297675714562/QUWgZ3Gk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1175469297675714562/QUWgZ3Gk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/5242/1569089262",
+			"profile_link_color": "002FA7",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "99CC33",
+			"profile_text_color": "3E4415",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "SoftBank’s Saudi-backed Vision Fund loses COO and four other partners https://t.co/fIpUXvSb5W"
+	}
+]
